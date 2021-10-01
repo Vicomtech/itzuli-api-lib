9eeb5598555df1ae18b723d81edb5e7866091838
pd-dev-vicomtech <ttprodkudeaketa01@gmail.com>
2021-10-01 11:10:59 +0200

    Fixed python package

---
+++ python/__init__.py
@@ -0,0 +1,1 @@
+from itzuli import Itzuli
--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="itzuli-pkg-vicomtech", # Replace with your own username
-    version="1.0.0",
+    version="1.0.1",
     author="Vicomtech.org",
     author_email="info@vicomtech.org",
     description="Itzuli API library",
